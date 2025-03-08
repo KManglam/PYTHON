b0a46c417a5579c3950e597aa787b9597e96a958
Kumar Manglam <kmglm04718@gmail.com>
2025-03-08 08:36:19 +0530

    Create p15.py

---
+++ p15.py
@@ -0,0 +1,14 @@
+# Numeric Example
+a_num = 10
+a_num = 20  # Creates a new integer object
+print(a_num)  # Output: 20
+
+# String Example
+a_str = "hello"
+a_str = "world"  # Creates a new string object
+print(a_str)  # Output: world
+
+# Tuple Example
+a_tuple = (1, 2, 3)
+# a_tuple[0] = 4  # This will raise a TypeError
+print(a_tuple)  # Output: (1, 2, 3)
