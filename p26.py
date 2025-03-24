0e701843cb54aa21529b59a6933ad91da29e4213
Kumar Manglam <kmglm04718@gmail.com>
2025-03-24 09:18:34 +0530

    Update p26.py

--- p26.py
+++ p26.py
@@ -1,10 +1,5 @@
 
-sample_dict = {
-    "name": "Kelly",
-    "age": 25,
-    "salary": 8000,
-    "city": "New york"
-}
+sample_dict = { "name": "Kelly", "age": 25, "salary": 8000,"city": "New york"}
 
 # Keys to remove
 keys = ["name", "salary"]
