b031468857d2b9d16ea261fee66e05be70f095f9
Kumar Manglam <kmglm04718@gmail.com>
2025-04-19 08:26:58 +0530

    Create p33.py

---
+++ p33.py
@@ -0,0 +1,11 @@
+# Write a Python program to Reverse a String
+
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print("Original string:", input_string)
+print("Reversed string:", reversed_string)
