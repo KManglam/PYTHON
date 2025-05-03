23677d1543abc06b42ca0639eff7f02a3f9402f0
Kumar Manglam <kmglm04718@gmail.com>
2025-05-03 08:17:05 +0530

    Update p35.py

--- p35.py
+++ p35.py
@@ -4,7 +4,6 @@ def is_palindrome(number):
     # Convert number to string for easy manipulation
     num_str = str(number)
     return num_str == num_str[::-1]
-  
 input_number = 12321
 if is_palindrome(input_number):
     print("The number is a palindrome.")
