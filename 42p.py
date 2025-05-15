5cde4997ed222ec67ce824858de3be352a7fe719
Kumar Manglam <kmglm04718@gmail.com>
2025-05-15 23:39:52 +0530

    Update 42p.py

--- 42p.py
+++ 42p.py
@@ -1,11 +1,5 @@
 arr = [45, 54, 76,123,25,123] # inputted array of numbers
-
-
-
-max_value = max(arr) # finding the max value of the max function
-
-
-
+max_value = max(arr) # finding the max value of the max functio
 print(f"The Maximum element in the array is: {max_value}") #printing the max element from array
 
 
