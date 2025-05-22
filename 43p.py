1fe24598a2d9c3bc31e1d44ae5dff8252a4d64a3
Kumar Manglam <kmglm04718@gmail.com>
2025-05-22 10:12:52 +0530

    Create 43p.py

---
+++ 43p.py
@@ -0,0 +1,15 @@
+p=float(input("Enter percentage:"))
+if(p>90):
+    grade='A'
+elif(p>80):
+    grade='B'
+elif(p>70):
+    grade='C'
+elif(p>60):
+    grade='D'
+elif(p>50):
+    grade='E'
+else:
+    grade='F'
+    
+print("Grade: ", grade)
