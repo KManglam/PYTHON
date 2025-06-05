0d231ca6f8bb7e798d0494f67907046a5bb13455
Kumar Manglam <kmglm04718@gmail.com>
2025-06-05 10:14:50 +0530

    Update 51p.py

--- 51p.py
+++ 51p.py
@@ -1,17 +1,9 @@
-/* This is the ListNode class definition
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class ListNode {
-public:
-	int data;
-	ListNode* next;
-
-	ListNode(int data) {
-		this->data = data;
-		this->next = NULL;
-	}
-};
-*/
-
-ListNode* reverseLinkedList (ListNode* head) {
-    // add your logic here
-}
+class MyClass(metaclass=Singleton):
+    pass
