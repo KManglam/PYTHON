cf22cfc535b560a482dbdec735d93b5670477a38
Kumar Manglam <kmglm04718@gmail.com>
2025-06-05 10:13:08 +0530

    Create 51p.py

---
+++ 51p.py
@@ -0,0 +1,17 @@
+/* This is the ListNode class definition
+
+class ListNode {
+public:
+	int data;
+	ListNode* next;
+
+	ListNode(int data) {
+		this->data = data;
+		this->next = NULL;
+	}
+};
+*/
+
+ListNode* reverseLinkedList (ListNode* head) {
+    // add your logic here
+}
